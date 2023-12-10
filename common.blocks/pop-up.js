1e566592abe35daa806a0cb1dce1ea2e40aa88f1
Rakhimzhan Rakhmayev <zhuba.sz@gmail.com>
2023-12-10 21:14:38 +0600

    added new block (Product Spotlight) with adaptive. Added pop-up function

---
+++ common.blocks/pop-up.js
@@ -0,0 +1,34 @@
+const dialog = document.getElementById('popUp')
+const dialogOpener = document.querySelector('.openDialogBtn')
+const dialogCloser = document.querySelector('.closeDialogBtn')
+
+function openModalAndLockScroll() {
+    dialog.showModal()
+    document.body.classList.add('scroll-lock')
+  }
+  
+  function returnScroll() {
+    document.body.classList.remove('scroll-lock')
+  }
+  
+  function close() {
+    dialog.close()
+    returnScroll()
+  }
+
+dialogOpener.addEventListener('click', openModalAndLockScroll)
+dialogCloser.addEventListener('click', (event) => {
+  event.stopPropagation()
+  close()
+})
+
+function closeOnBackDropClick({ currentTarget, target }) {
+    const dialog = currentTarget
+    const isClickedOnBackDrop = target === dialog
+    if (isClickedOnBackDrop) {
+      dialog.close()
+      returnScroll()
+    }
+  }
+  
+  dialog.addEventListener('click', closeOnBackDropClick)

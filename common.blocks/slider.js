6d322f574f3fd5d4b73801d89955abdaa0fd3e99
Rakhimzhan Rakhmayev <zhuba.sz@gmail.com>
2023-12-12 22:08:15 +0600

    added slider functional with the help SwiperJS

---
+++ common.blocks/slider.js
@@ -0,0 +1,11 @@
+new Swiper('.sliders', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+    },
+});

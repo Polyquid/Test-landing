1d6aead6284020182fa78df8bc5b2f90399d4b45
Rakhimzhan Rakhmayev <zhuba.sz@gmail.com>
2023-12-14 22:19:29 +0600

    deploy on github-pages

--- common.blocks/form.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector('.form')
-form.addEventListener('submit', 
-    function(e){
-            e.preventDefault();
-        }
-)
-
-
--- index.js
+++
@@ -1,6 +0,0 @@
-import './common.blocks/pop-up.js'
-import './common.blocks/slider.js'
-import './common.blocks/smooth-scroll.js'
-import './common.blocks/form.js'
-import './index.css'
-// import html from "./index.html"

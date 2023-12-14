9deb8b8519db4c29828443d37bdd8420e55d6307
Rakhimzhan Rakhmayev <zhuba.sz@gmail.com>
2023-12-14 21:49:59 +0600

    Using Webpack

--- common.blocks/slider.js
+++ common.blocks/slider.js
@@ -1,13 +1,20 @@
-new Swiper('.sliders', {
+import Swiper from 'swiper';
+import { Keyboard, Pagination } from 'swiper/modules';
+import 'swiper/css';
+// import 'swiper/css/keyboard';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.sliders', {
+    modules: [Keyboard, Pagination],
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
     spaceBetween: 10,
     keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-    pageUpDown: true,
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
     },
     loop: true,
 });
---
+++ index.js
@@ -0,0 +1,6 @@
+import './common.blocks/pop-up.js'
+import './common.blocks/slider.js'
+import './common.blocks/smooth-scroll.js'
+import './common.blocks/form.js'
+import './index.css'
+// import html from "./index.html"
